358d4b3690cc9203e25f2a23bbc13abeb3a24248
laura.carlotta <carlotta.custodio@gmail.com>
2020-07-12 15:03:34 -0300

    Deu erro porque adicionei código, então refiz

--- A.js
+++ A.js
@@ -1,2 +1,4 @@
 /* <= este aquivo não estava sendo monitorado pelo git **** Comit = salvando os serviços 
 **** -m = você comenta a mensagem do que você alterou naquilo comitado */
+/* Conta uma história do que você fez */
+/*a = add = adicionado ao git */
